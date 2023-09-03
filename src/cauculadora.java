a7d8bc3588d9d1b1831fbc3192b10ced1ec7c454
felipelizardo <106035259+felipelizardo@users.noreply.github.com>
2023-09-03 09:18:34 -0300

    projeto java

---
+++ src/cauculadora.java
@@ -0,0 +1,15 @@
+/**
+* @author Felipe
+* @version 1.0
+* @since 03/09/2023
+*/
+public class cauculadora {
+   /** 
+    * @param numeroUm paramentro para receber o primeiro numero
+    * @param numeroDois parametro para receber o segundo numero
+    * @return retornar a soma entre o pimeiro mais o segundo parametro
+*/
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
